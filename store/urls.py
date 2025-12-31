84760b24b621c1514323e79e831e62a2505cf5ba
Firass Zaoui <fzaoui636@gmail.com>
2025-12-31 19:13:36 +0100

    Reset project to stable working state

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, CategoryViewSet
-
-router = DefaultRouter()
-router.register(r'items', ItemViewSet, basename='item')
-router.register(r'categories', CategoryViewSet, basename='category')
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # Leave this empty for now
 ]
