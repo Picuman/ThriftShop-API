6cd2179dfccdfd5e62832e69cf592e25db09e014
Firass Zaoui <fzaoui636@gmail.com>
2025-12-31 02:11:17 +0100

    Implement Item serializers, ViewSets, and API routes

--- store/views.py
+++ store/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from .models import Item, Category
+from .serializers import ItemSerializer, CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Public View: List all categories
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.AllowAny]
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    CRUD View:
+    - List/Retrieve: Allow Any (Public)
+    - Create/Update/Delete: Authenticated Users only
+    """
+    queryset = Item.objects.filter(is_sold=False) 
+    serializer_class = ItemSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description', 'category__name']
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
