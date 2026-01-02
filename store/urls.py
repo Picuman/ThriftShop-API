3d80fe486d90bc23e4554c3da879bd56bdc2a74c
Firass Zaoui <fzaoui636@gmail.com>
2026-01-02 19:11:59 +0100

    Added Category API endpoints and linked items

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
+    
     path('items/', views.ItemList.as_view(), name='item-list'),
+    path('items/<int:pk>/', views.ItemDetail.as_view(), name='item-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,16 +1,30 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Category, Item
 from .serializers import CategorySerializer, ItemSerializer
 
-# View for listing all categories or creating a new one
+# --- CATEGORY VIEWS ---
+
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+# --- ITEM VIEWS ---
 
 class ItemList(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # Add this method:
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
