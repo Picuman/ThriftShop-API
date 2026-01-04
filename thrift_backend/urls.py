3ecfc49ccf4c9a4a950c6a09a45671e1812b2210
Firass Zaoui <fzaoui636@gmail.com>
2026-01-04 01:48:04 +0100

    Final poilsh: Added Swagger API documentation

--- thrift_backend/urls.py
+++ thrift_backend/urls.py
@@ -3,11 +3,31 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Documentation Imports
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Thrift Shop API",
+      default_version='v1',
+      description="API documentation for the Capstone Thrift Shop Project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@thrift.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
+    
+    # The Documentation URL
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
-# This part tells Django: "If we are in Debug mode, serve images from the MEDIA folder"
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
